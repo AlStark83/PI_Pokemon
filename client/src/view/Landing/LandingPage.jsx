c06d6c825fa986c6246683e0ae350b4c99580672
Alberto Guerrero Covarrubias <a.guerrero.c.83@gmail.com>
2023-03-24 11:02:07 -0600

    Landing CSS
    Button color & shape look alike pokemon Go ok

--- client/src/view/Landing/LandingPage.jsx
+++ client/src/view/Landing/LandingPage.jsx
@@ -5,16 +5,24 @@ import styles from "./Landing.module.css";
 export default function LandingPage() {
 	return (
 		<div className={styles.landing}>
-			<img
-				src="https://fontmeme.com/permalink/230128/d791fc1496ece52131cfa65e9dc9bf5c.png"
-				alt="anadir-bordes"
-				border="0"
-			/>
-
-			<h1>Go to Catch'em All</h1>
-			<Link to="/home">
-				<button className={styles.button}>Enter</button>
-			</Link>
+			<div className={styles.image}>
+				<img
+					src="https://res.cloudinary.com/dfivzhcuw/image/upload/v1679021482/Squirtle_pffwsm.png"
+					alt="Squirtle"
+				/>
+			</div>
+			<div className={styles.col2}>
+				<div className={styles.logo}>
+					<img
+						src="https://fontmeme.com/permalink/230128/d791fc1496ece52131cfa65e9dc9bf5c.png"
+						alt="Pokemon Logo"
+					/>
+				</div>
+				<h1>Go to Catch'em All</h1>
+				<Link to="/home">
+					<button className={styles.button}>Enter</button>
+				</Link>
+			</div>
 		</div>
 	);
 }
